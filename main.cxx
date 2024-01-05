a35b657c519279ecbfcc14557135028ad2986b8f
Subhajit Sahu <wolfram77@gmail.com>
2024-01-05 22:55:09 +0530

    :bug: with experiment

--- main.cxx
+++ main.cxx
@@ -1,7 +1,9 @@
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
+#include <utility>
 #include <vector>
-#include <random>
+#include <iostream>
 #include "inc/main.hxx"
 
 using namespace std;
@@ -32,141 +34,53 @@ using namespace std;
 
 
 #pragma region METHODS
-#pragma region EXPERIMENTAL SETUP
-/**
- * Run a function on each batch update, with a specified range of batch sizes.
- * @param x original graph
- * @param rnd random number generator
- * @param fn function to run on each batch update
- */
-template <class G, class R, class F>
-inline void runBatches(const G& x, R& rnd, F fn) {
-  auto fl = [](auto u) { return true; };
-  double d = BATCH_DELETIONS_BEGIN;
-  double i = BATCH_INSERTIONS_BEGIN;
-  for (int epoch=0;; ++epoch) {
-    for (int r=0; r<REPEAT_BATCH; ++r) {
-      auto y  = duplicate(x);
-      for (int sequence=0; sequence<BATCH_LENGTH; ++sequence) {
-        auto deletions  = generateEdgeDeletions (rnd, y, size_t(d * x.size()/2), 1, x.span()-1, true);
-        auto insertions = generateEdgeInsertions(rnd, y, size_t(i * x.size()/2), 1, x.span()-1, true, None());
-        tidyBatchUpdateU(deletions, insertions, y);
-        applyBatchUpdateOmpU(y, deletions, insertions);
-        addSelfLoopsOmpU(y, None(), fl);
-        auto yt = transposeWithDegreeOmp(y);
-        fn(y, yt, d, deletions, i, insertions, sequence, epoch);
-      }
-    }
-    if (d>=BATCH_DELETIONS_END && i>=BATCH_INSERTIONS_END) break;
-    d BATCH_DELETIONS_STEP;
-    i BATCH_INSERTIONS_STEP;
-    d = min(d, double(BATCH_DELETIONS_END));
-    i = min(i, double(BATCH_INSERTIONS_END));
-  }
-}
-
-
-/**
- * Run a function on each number of threads, for a specific epoch.
- * @param epoch epoch number
- * @param fn function to run on each number of threads
- */
-template <class F>
-inline void runThreadsWithBatch(int epoch, F fn) {
-  int t = NUM_THREADS_BEGIN;
-  for (int l=0; l<epoch && t<=NUM_THREADS_END; ++l)
-    t NUM_THREADS_STEP;
-  omp_set_num_threads(t);
-  fn(t);
-  omp_set_num_threads(MAX_THREADS);
-}
-
-
-/**
- * Run a function on each number of threads, with a specified range of thread counts.
- * @param fn function to run on each number of threads
- */
-template <class F>
-inline void runThreadsAll(F fn) {
-  for (int t=NUM_THREADS_BEGIN; t<=NUM_THREADS_END; t NUM_THREADS_STEP) {
-    omp_set_num_threads(t);
-    fn(t);
-    omp_set_num_threads(MAX_THREADS);
-  }
-}
-
-
-/**
- * Run a function on each number of threads, with a specified range of thread counts or for a specific epoch (depending on NUM_THREADS_MODE).
- * @param epoch epoch number
- * @param fn function to run on each number of threads
- */
-template <class F>
-inline void runThreads(int epoch, F fn) {
-  if (NUM_THREADS_MODE=="with-batch") runThreadsWithBatch(epoch, fn);
-  else runThreadsAll(fn);
-}
-#pragma endregion
-
-
-
-
 #pragma region PERFORM EXPERIMENT
 /**
  * Perform the experiment.
- * @param x original graph
- * @param xt transposed graph
+ * @param x input graph
+ * @param xt transpose of input graph
+ * @param fstream input file stream
+ * @param rows number of rows/vetices in the graph
+ * @param size number of lines/edges (temporal) in the graph
  */
 template <class G, class H>
-void runExperiment(const G& x, const H& xt) {
-  using  K = typename G::key_type;
-  using  V = TYPE;
-  random_device dev;
-  default_random_engine rnd(dev());
-  int repeat = REPEAT_METHOD;
+void runExperiment(G& x, H& xt, istream& fstream, size_t rows, size_t size) {
+  using K = typename G::key_type;
+  using V = TYPE;
+  int    repeat = REPEAT_METHOD;
+  double batchFraction = 1e-4;
+  auto   fl = [](auto u) { return true; };
   // Follow a specific result logging format, which can be easily parsed later.
-  auto glog  = [&](const auto& ans, const auto& ref, const char *technique, int numThreads, double deletionsf, double insertionsf, V frontierTolerance, V pruneTolerance) {
+  auto glog  = [&](const auto& ans, const auto& ref, const char *technique, double deletionsf, double insertionsf, int batchIndex) {
     auto err = l1NormDeltaOmp(ans.ranks, ref.ranks);
     printf(
-      "{-%.3e/+%.3e batchf, %03d threads} -> {%09.1fms, %09.1fms init, %09.1fms mark, %09.1fms comp, %03d iter, %.2e err} %s\n",
-      deletionsf, insertionsf, numThreads,
+      "{-%.3e/+%.3e batchf, %03d batchi} -> {%09.1fms, %09.1fms init, %09.1fms mark, %09.1fms comp, %03d iter, %.2e err} %s\n",
+      deletionsf, insertionsf, batchIndex,
       ans.time, ans.initializationTime, ans.markingTime, ans.computationTime, ans.iterations, err, technique
     );
   };
+  V tolerance = 1e-10;
+  V frontierTolerance = 1e-15;
+  V pruneTolerance    = 1e-15;
   // Get ranks of vertices on original graph (static).
   auto r0 = pagerankStaticOmp(xt, PagerankOptions<V>(1, 1e-100));
   // Get ranks of vertices on updated graph (dynamic).
-  runBatches(x, rnd, [&](const auto& y, const auto& yt, double deletionsf, const auto& deletions, double insertionsf, const auto& insertions, int sequence, int epoch) {
-    runThreads(epoch, [&](int numThreads) {
-      auto flog = [&](const auto& ans, const auto& ref, const char *technique, V frontierTolerance, V pruneTolerance) {
-        glog(ans, ref, technique, numThreads, deletionsf, insertionsf, frontierTolerance, pruneTolerance);
-      };
-      V tolerance = 1e-10;
-      V frontierTolerance = 1e-15;
-      V pruneTolerance = 1e-15;
-      auto s0 = pagerankStaticOmp(yt, PagerankOptions<V>(1, 1e-100));
-      // Find multi-threaded OpenMP-based Static PageRank.
-      auto a0 = pagerankStaticOmp<false>(yt, PagerankOptions<V>(repeat, tolerance, frontierTolerance));
-      flog(a0, s0, "pagerankStaticOmp", frontierTolerance, 0.0);
-      auto b0 = pagerankPruneStaticOmp<false, true>(y, yt, PagerankOptions<V>(repeat, tolerance, frontierTolerance, pruneTolerance));
-      flog(b0, s0, "pagerankPruneStaticOmp", frontierTolerance, pruneTolerance);
-      // Find multi-threaded OpenMP-based Naive-dynamic PageRank.
-      auto a1 = pagerankNaiveDynamicOmp<true>(yt, &r0.ranks, {repeat, tolerance, frontierTolerance});
-      flog(a1, s0, "pagerankNaiveDynamicOmp", frontierTolerance, 0.0);
-      auto b1 = pagerankPruneNaiveDynamicOmp<true, true>(y, yt, &r0.ranks, {repeat, tolerance, frontierTolerance, pruneTolerance});
-      flog(b1, s0, "pagerankPruneNaiveDynamicOmp", frontierTolerance, pruneTolerance);
-      // Find multi-threaded OpenMP-based Frontier-based Dynamic PageRank.
-      auto a2 = pagerankDynamicFrontierOmp<true, true>(x, xt, y, yt, deletions, insertions, &r0.ranks, {repeat, tolerance, frontierTolerance});
-      flog(a2, s0, "pagerankDynamicFrontierOmp", frontierTolerance, 0.0);
-      auto b2 = pagerankPruneDynamicFrontierOmp<true, true>(x, xt, y, yt, deletions, insertions, &r0.ranks, {repeat, tolerance, frontierTolerance, pruneTolerance});
-      flog(b2, s0, "pagerankPruneDynamicFrontierOmp", frontierTolerance, pruneTolerance);
-      // Find multi-threaded OpenMP-based Traversal-based Dynamic PageRank.
-      auto a3 = pagerankDynamicTraversalOmp<true>(x, xt, y, yt, deletions, insertions, &r0.ranks, {repeat, tolerance, frontierTolerance});
-      flog(a3, s0, "pagerankDynamicTraversalOmp", frontierTolerance, 0.0);
-      auto b3 = pagerankPruneDynamicTraversalOmp<true, true>(x, xt, y, yt, deletions, insertions, &r0.ranks, {repeat, tolerance, frontierTolerance, pruneTolerance});
-      flog(b3, s0, "pagerankPruneDynamicTraversalOmp", frontierTolerance, pruneTolerance);
-    });
-  });
+  for (int batchIndex=0; batchIndex<BATCH_LENGTH; ++batchIndex) {
+    auto y = duplicate(x);
+    readTemporalOmpW(y, fstream, false, false, rows, size_t(batchFraction * size));
+    y  = addSelfLoopsOmp(y, None(), fl);
+    yt = transposeWithDegreeOmp(y);
+    auto a0 = pagerankStaticOmp<false>(yt, PagerankOptions<V>(repeat, tolerance, frontierTolerance));
+    glog(a0, r0, "pagerankStaticOmp", 0.0, 0.0, batchIndex);
+    auto a1 = pagerankNaiveDynamicOmp<true>(yt, &r0.ranks, {repeat, tolerance, frontierTolerance});
+    glog(a1, r0, "pagerankNaiveDynamicOmp", 0.0, 0.0, batchIndex);
+    auto a2 = pagerankDynamicFrontierOmp<true, true>(x, xt, y, yt, None(), None(), &r0.ranks, {repeat, tolerance, frontierTolerance});
+    glog(a2, r0, "pagerankDynamicFrontierOmp", 0.0, 0.0, batchIndex);
+    auto a3 = pagerankDynamicTraversalOmp<true>(x, xt, y, yt, None(), None(), &r0.ranks, {repeat, tolerance, frontierTolerance});
+    glog(a3, r0, "pagerankDynamicTraversalOmp", 0.0, 0.0, batchIndex);
+    x  = move(y);
+    xt = move(yt);
+  }
 }
 
 
@@ -177,16 +91,19 @@ void runExperiment(const G& x, const H& xt) {
  * @returns zero on success, non-zero on failure
  */
 int main(int argc, char **argv) {
-  char *file = argv[1];
+  char *file  = argv[1];
+  size_t rows = strtoull(argv[2], nullptr, 10);
+  size_t size = strtoull(argv[3], nullptr, 10);
   omp_set_num_threads(MAX_THREADS);
   LOG("OMP_NUM_THREADS=%d\n", MAX_THREADS);
   LOG("Loading graph %s ...\n", file);
   DiGraph<uint32_t> x;
-  readMtxOmpW(x, file); LOG(""); println(x);
+  ifstream fstream(file);
+  readTemporalOmpW(x, fstream, false, false, rows, size_t(0.80 * size)); LOG(""); println(x); printf(" (80%)\n");
   auto fl = [](auto u) { return true; };
   x = addSelfLoopsOmp(x, None(), fl);  LOG(""); print(x);  printf(" (selfLoopAllVertices)\n");
   auto xt = transposeWithDegreeOmp(x); LOG(""); print(xt); printf(" (transposeWithDegree)\n");
-  runExperiment(x, xt);
+  runExperiment(x, xt, fstream, rows, size);
   printf("\n");
   return 0;
 }
--- main.sh
+++ main.sh
@@ -9,6 +9,7 @@ if [[ "$DOWNLOAD" != "0" ]]; then
   rm -rf $src
   git clone https://github.com/puzzlef/$src
   cd $src
+  git checkout measure-temporal
 fi
 
 # Fixed config
@@ -19,17 +20,6 @@ fi
 # Parameter sweep for batch (randomly generated)
 : "${BATCH_UNIT:=%}"
 : "${BATCH_LENGTH:=1}"
-: "${BATCH_DELETIONS_BEGIN:=0.00000005}"
-: "${BATCH_DELETIONS_END:=0.05}"
-: "${BATCH_DELETIONS_STEP:=*=10}"
-: "${BATCH_INSERTIONS_BEGIN:=0.00000005}"
-: "${BATCH_INSERTIONS_END:=0.05}"
-: "${BATCH_INSERTIONS_STEP:=*=10}"
-# Parameter sweep for number of threads
-: "${NUM_THREADS_MODE:=all}"
-: "${NUM_THREADS_BEGIN:=64}"
-: "${NUM_THREADS_END:=64}"
-: "${NUM_THREADS_STEP:=*=2}"
 # Define macros (dont forget to add here)
 DEFINES=(""
 "-DTYPE=$TYPE"
@@ -38,34 +28,13 @@ DEFINES=(""
 "-DREPEAT_METHOD=$REPEAT_METHOD"
 "-DBATCH_UNIT=\"$BATCH_UNIT\""
 "-DBATCH_LENGTH=$BATCH_LENGTH"
-"-DBATCH_DELETIONS_BEGIN=$BATCH_DELETIONS_BEGIN"
-"-DBATCH_DELETIONS_END=$BATCH_DELETIONS_END"
-"-DBATCH_DELETIONS_STEP=$BATCH_DELETIONS_STEP"
-"-DBATCH_INSERTIONS_BEGIN=$BATCH_INSERTIONS_BEGIN"
-"-DBATCH_INSERTIONS_END=$BATCH_INSERTIONS_END"
-"-DBATCH_INSERTIONS_STEP=$BATCH_INSERTIONS_STEP"
-"-DNUM_THREADS_MODE=\"$NUM_THREADS_MODE\""
-"-DNUM_THREADS_BEGIN=$NUM_THREADS_BEGIN"
-"-DNUM_THREADS_END=$NUM_THREADS_END"
-"-DNUM_THREADS_STEP=$NUM_THREADS_STEP"
 )
 
 # Run
 g++ ${DEFINES[*]} -std=c++17 -O3 -fopenmp main.cxx -o "a$1.out"
 # stdbuf --output=L ./"a$1.out" ~/Data/soc-Epinions1.mtx  2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/indochina-2004.mtx  2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/uk-2002.mtx         2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/arabic-2005.mtx     2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/uk-2005.mtx         2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/webbase-2001.mtx    2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/it-2004.mtx         2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/sk-2005.mtx         2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/com-LiveJournal.mtx 2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/com-Orkut.mtx       2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/asia_osm.mtx        2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/europe_osm.mtx      2>&1 | tee -a "$out"
-stdbuf --output=L ./"a$1.out" ~/Data/kmer_A2a.mtx        2>&1 | tee -a "$out"x
-stdbuf --output=L ./"a$1.out" ~/Data/kmer_V1r.mtx        2>&1 | tee -a "$out"x
+stdbuf --output=L ./"a$1.out" ~/Data/wiki-talk-temporal.txt 1140149 7833140  3309592  2>&1 | tee -a "$out"
+stdbuf --output=L ./"a$1.out" ~/Data/sx-stackoverflow.txt   2601977 63497050 36233450 2>&1 | tee -a "$out"
 
 # Signal completion
 curl -X POST "https://maker.ifttt.com/trigger/puzzlef/with/key/${IFTTT_KEY}?value1=$src$1"
