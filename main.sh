44ea5f737850eae5ff2ae2c1ba5889d3a8be04e5
Subhajit Sahu <wolfram77@gmail.com>
2024-01-24 20:36:53 +0530

    :bug: setup strong scaling expt

--- main.sh
+++ main.sh
@@ -9,16 +9,17 @@ if [[ "$DOWNLOAD" != "0" ]]; then
   rm -rf $src
   git clone https://github.com/puzzlef/$src
   cd $src
+  git checkout -b strong-scaling-temporal
 fi
 
 # Fixed config
 : "${TYPE:=double}"
 : "${MAX_THREADS:=64}"
-: "${REPEAT_BATCH:=5}"
+: "${REPEAT_BATCH:=1}"
 : "${REPEAT_METHOD:=1}"
 # Parameter sweep for batch (randomly generated)
 : "${BATCH_UNIT:=%}"
-: "${BATCH_LENGTH:=100}"
+: "${BATCH_LENGTH:=5}"
 # Parameter sweep for number of threads
 : "${NUM_THREADS_MODE:=all}"
 : "${NUM_THREADS_BEGIN:=64}"
@@ -43,17 +44,28 @@ g++ ${DEFINES[*]} -std=c++17 -O3 -fopenmp main.cxx
 
 # Run on each temporal graph, with specified batch fraction
 runEach() {
-stdbuf --output=L ./a.out ~/Data/sx-mathoverflow.txt    248180   506550   239978   "$1" 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/sx-askubuntu.txt       1593160  964437   596933   "$1" 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/sx-superuser.txt       1940850  1443339  924886   "$1" 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/wiki-talk-temporal.txt 11401490 7833140  3309592  "$1" 2>&1 | tee -a "$out"
-stdbuf --output=L ./a.out ~/Data/sx-stackoverflow.txt   26019770 63497050 36233450 "$1" 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/sx-mathoverflow.txt    248180   506550   239978   "$1" "$2" 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/sx-askubuntu.txt       1593160  964437   596933   "$1" "$2" 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/sx-superuser.txt       1940850  1443339  924886   "$1" "$2" 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/wiki-talk-temporal.txt 11401490 7833140  3309592  "$1" "$2" 2>&1 | tee -a "$out"
+stdbuf --output=L ./a.out ~/Data/sx-stackoverflow.txt   26019770 63497050 36233450 "$1" "$2" 2>&1 | tee -a "$out"
 }
 
 # Run with different batch fractions
-runEach "0.00001"
-runEach "0.0001"
-runEach "0.001"
+runBatch() {
+runEach "0.00001" "$1"
+runEach "0.0001"  "$1"
+runEach "0.001"   "$1"
+}
+
+# Run with different number of threads
+runBatch "1"
+runBatch "2"
+runBatch "4"
+runBatch "8"
+runBatch "16"
+runBatch "32"
+runBatch "64"
 
 # Signal completion
 curl -X POST "https://maker.ifttt.com/trigger/puzzlef/with/key/${IFTTT_KEY}?value1=$src$1"
